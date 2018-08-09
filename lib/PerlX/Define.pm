4ff197b84ab41f464887544d5290a32680c032a2
Toby Inkster <mail@tobyinkster.co.uk>
2018-08-09 15:16:26 +0100

    bump version; changelog

--- lib/PerlX/Define.pm
+++ lib/PerlX/Define.pm
@@ -6,7 +6,7 @@ no warnings qw(void once uninitialized numeric);
 package PerlX::Define;
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.100';
+our $VERSION   = '0.101';
 
 use B ();
 use Keyword::Simple ();
